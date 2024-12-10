e0fcf9048544e29acf110c9a9db1cff5b820c2c0
Rishat Tarisov <r.tarisov@gmail.com>
2024-12-10 20:44:24 +0500

    fix: pass 47

--- tester.js
+++ tester.js
@@ -1,22 +1,20 @@
-function findLongestIncreasingSubsequence(nums) {
-  const r = nums.reduce(
-    (a, c) => {
-      const obj = a;
-      if (obj.prev < c) {
-        obj.prev = c;
-        obj.cnt += 1;
-      } else {
-        obj.prev = c;
-        obj.cnt = 1;
-      }
-      obj.max = Math.max(a.max, a.cnt);
-      return a;
-    },
-    { prev: Number.MAX_VALUE, max: Number.MIN_VALUE, cnt: 1 }
-  );
-  return r.max;
+/**
+ * Shifts an array by n positions. If n is negative, the array is shifted to the left;
+ * if positive, it is shifted to the right.
+ *
+ * @param {array} arr - The array to be shifted.
+ * @param {number} n - The number of positions to shift the array elements.
+ * @return {array} - The shifted array.
+ *
+ * @example
+ *    shiftArray([1, 2, 3, 4, 5], 2) => [4, 5, 1, 2, 3]
+ *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
+ *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
+ */
+function shiftArray(arr, n) {
+  return arr.slice(-n).concat(arr.slice(0, -n));
 }
 
-console.log(
-  findLongestIncreasingSubsequence([10, 22, 9, 33, 21, 50, 41, 60, 80])
-);
+console.log(shiftArray([1, 2, 3, 4, 5], 2));
+console.log(shiftArray(['a', 'b', 'c', 'd'], -1));
+console.log(shiftArray([10, 20, 30, 40, 50], -3));
