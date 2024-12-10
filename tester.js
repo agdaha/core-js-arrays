8402d595954ba88233a2b9f738747e06279aec3e
Rishat Tarisov <r.tarisov@gmail.com>
2024-12-10 20:48:41 +0500

    fix: pass 48

--- tester.js
+++ tester.js
@@ -1,20 +1,18 @@
-/**
- * Shifts an array by n positions. If n is negative, the array is shifted to the left;
- * if positive, it is shifted to the right.
- *
- * @param {array} arr - The array to be shifted.
- * @param {number} n - The number of positions to shift the array elements.
- * @return {array} - The shifted array.
- *
- * @example
- *    shiftArray([1, 2, 3, 4, 5], 2) => [4, 5, 1, 2, 3]
- *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
- *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
- */
-function shiftArray(arr, n) {
-  return arr.slice(-n).concat(arr.slice(0, -n));
+function sortDigitNamesByNumericOrder(arr) {
+  const sortArr = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.sort((a, b) => sortArr.indexOf(a) - sortArr.indexOf(b));
 }
 
-console.log(shiftArray([1, 2, 3, 4, 5], 2));
-console.log(shiftArray(['a', 'b', 'c', 'd'], -1));
-console.log(shiftArray([10, 20, 30, 40, 50], -3));
+console.log(sortDigitNamesByNumericOrder(['nine', 'one']));
+console.log(sortDigitNamesByNumericOrder(['one', 'one', 'one', 'zero']));
